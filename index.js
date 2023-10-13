d211879a21c7af6ebd274c3524c74145fd6874ff
Jonathan Cunningham <jnidaokefantageunity@gmail.com>
2023-10-13 07:10:22 -0400

    env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send(`Yo! Env = ${process.env.TEST}`)
 })
 app.listen(process.env.PORT || 3000)
