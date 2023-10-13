e94daa7bedae140a3089c99478951420bd85008b
Jonathan Cunningham <jnidaokefantageunity@gmail.com>
2023-10-13 08:01:14 -0400

    cors

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
+const cors = require('cors');
+const express = require('express');
+const app = express();
 
-app.use(express.json());
+app.use(express.json(), cors({ origin: '*' }));
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send(`Yo! Env = ${process.env.TEST}`)
-})
+});
 
 app.get("/open", (req, res, next) => {
   res.json(["Hello", "World!"]);
 });
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
