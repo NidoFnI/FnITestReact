69ba2d4763c543cde4f887054661f5a1d415ed41
Jonathan Cunningham <jnidaokefantageunity@gmail.com>
2023-10-13 07:32:04 -0400

    use and open

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
+app.use(express.json());
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send(`Yo! Env = ${process.env.TEST}`)
 })
+
+app.get("/open", (req, res, next) => {
+  res.json(["Hello", "World!"]);
+});
+
 app.listen(process.env.PORT || 3000)
